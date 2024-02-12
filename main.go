34348d865ee2a32e2ed2e00b2acdffbca48381cd
Saireddy <sainath.reddy@digite.com>
2024-02-12 22:37:23 +0530

    .

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
-// quick sort
-
 package main
 
 import "fmt"
 
-func main(){
-	arr := []int{4,6,2,5,7,9,1,3}
-	n := len(arr)
-	quicksort(arr, 0, n-1)
-	fmt.Println(arr)
-}
+var name string = "adfs"
 
-func quicksort(arr []int, low int, high int){
-	if low >= high {
-		return
-	}
-	i := low 
-	j := high 
-	pivot := arr[i]
-	pivotIndex := i 
+func main() {
 
-	for i < j {
-		for arr[i] <= pivot && i < j {
-			i++
-		}
-		for arr[j] >= pivot && i < j {
-			j--
-		}
-		if i < j {
-			arr[i], arr[j] = arr[j], arr[i]
-		}
-	}
-	j--
-	arr[j], arr[pivotIndex] = arr[pivotIndex], arr[j]
-	quicksort(arr, low, pivotIndex-1)
-	quicksort(arr, pivotIndex+1, high)
+	var name string
+	fmt.Scanf("%s", &name)
+	fmt.Println(name)
 }
