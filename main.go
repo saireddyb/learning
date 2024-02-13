f881fbc288a515feb82c9fe7b2bb1e397c300ed1
Saireddy <sainath.reddy@digite.com>
2024-02-13 11:54:38 +0530

    .

--- main.go
+++ main.go
@@ -1,26 +1,8 @@
-// Left Rotate an array by one place
-
 package main
 
 import "fmt"
 
 func main() {
 	arr := []int{1,2,3,4,5,6,7}
-	p := 1
-	rotaten(arr, p)
 	fmt.Println(arr)
-}
-
-func rotaten(arr []int, p int) {
-	var temp []int = make([]int, p)
-	for i:=0; i<p; i++{
-		temp[i] = arr[i]
-	}
-	n := len(arr)
-	for i:=p; i<n; i++ {
-		arr[i-p]= arr[i]
-	}
-	for i:= n-p; i < n; i++{
-		arr[i] = temp[i-(n-p)]
-	}
 }
