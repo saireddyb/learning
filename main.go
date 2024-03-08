9822f5ad67cca8844db28d7199fa7f01dabfe31f
Saireddy <sainath.reddy@digite.com>
2024-03-08 09:59:23 +0530

    .

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 func main() {
-	arr := []int{1,2,3,4,5,6,7}
-	fmt.Println(arr)
+	// Create a new context being cancelled in 5 seconds.
+	ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+	// Start a new goroutine whose lifetime's attached to ctx.
+	go Task(ctx)
+	fmt.Println("main")
+	time.Sleep(time.Second * 10)
+}
+
+func Task(ctx context.Context)  {
+	time.Sleep(time.Second * 6)
+	fmt.Println("hello")
+	time.Sleep(time.Second * 4)
+	fmt.Println("hello")
 }
